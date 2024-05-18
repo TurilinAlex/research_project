d5355f95a033a828cac45084dcd80a2d4691582a
turilin <turilin@primatlab.com>
2024-05-18 11:09:10 +0300

    Update import

---
+++ trading_algorithm/__init__.py
@@ -0,0 +1,4 @@
+from .max_extremes.max_extremes import MaxExtremes
+from .min_extremes.min_extremes import MinExtremes
+from .union_extremes.union_extremes import UnionExtremes
+from .utils import max_extremum, min_extremum
